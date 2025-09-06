9726ac43cc01bf69cefe592ea773b4a1309b3741
Gavin Borne <gavin.bor0925@gmail.com>
2025-09-06 18:16:20 -0400

    add arg_validation.py

---
+++ src/ironclad/arg_validation.py
@@ -0,0 +1,55 @@
+"""arg_validation.py
+
+Argument validation functions, including type and value enforcing.
+"""
+
+import functools
+import inspect
+
+
+def enforce_types(**type_map):
+    """Decorator that enforces the types of function parameters.
+
+    Arguments
+    ---------
+    type_map : Dict[str, Type | Tuple[Type, ...]]
+        A dictionary mapping argument names to expected type(s)
+    """
+
+    def decorator(func):
+        sig = inspect.signature(func)
+
+        # validate all arguments given exist in the function signature
+        for arg_name in type_map:
+            if arg_name not in sig.parameters:
+                raise ValueError(
+                    f"Argument to enforce '{arg_name}' not found "
+                    + f"in function signature of '{func.__name__}'"
+                )
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            bound_args = sig.bind(*args, **kwargs)
+            bound_args.apply_defaults()
+
+            for arg_name, type_or_tuple in type_map.items():
+                arg_value = bound_args.arguments[arg_name]
+                if not isinstance(arg_value, type_or_tuple):
+                    # convert the type(s) to a string
+                    # if a tuple of types, join each type around " or "
+                    type_string = (
+                        type_or_tuple.__name__
+                        if isinstance(type_or_tuple, type)
+                        else " or ".join(t for t in type_or_tuple)
+                    )
+
+                    raise TypeError(
+                        f"Argument '{arg_name}' must be of type '{type_string}' or a subclass, "
+                        + f"not '{type(arg_value).__name__}'"
+                    )
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
