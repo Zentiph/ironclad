efa07796d518d9903b6c422ac679b1c33dc6b3cb
Gavin Borne <gavin.bor0925@gmail.com>
2025-09-06 20:53:42 -0400

    add dunder vars to init

--- src/ironclad/__init__.py
+++ src/ironclad/__init__.py
@@ -8,3 +8,14 @@ A lightweight toolkit for enforcing strict runtime contracts
 :copyright: (c) 2025-present Zentiph
 :license: MIT; see LICENSE for more details.
 """
+
+__all__ = ["enforce_types"]
+
+__title__ = "ironclad"
+__author__ = "Zentiph"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025-present Zentiph"
+# __version__ =
+# TODO: add version parse from version info
+
+from .arg_validation import enforce_types
