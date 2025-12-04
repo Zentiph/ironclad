27b2d5b0a8486cfaaacc47ca18ee3f334a5c512f
Gavin Borne <zentiphdev@gmail.com>
2025-12-04 17:25:12 -0500

    add tests for xor and implies

--- tests/predicates/test_predicate.py
+++ tests/predicates/test_predicate.py
@@ -68,31 +68,51 @@ def test_with_changers() -> None:
 def test_and() -> None:
     pred1 = Predicate(is_pos, "is positive")
     pred2 = Predicate[int](lambda x: not is_pos(x), "is not positive")
-    combined = pred1 & pred2
 
-    assert combined(1) is False, "ANDed predicate did not return the expected value."
-    assert combined(-1) is False, "ANDed predicate did not return the expected value."
+    combined = pred1 & pred2
+    assert combined(1) is False
+    assert combined(-1) is False
     assert combined.render_msg() == "(is positive) and (is not positive)"
 
 
 def test_or() -> None:
     pred1 = Predicate(is_pos, "is positive")
     pred2 = Predicate[int](lambda x: x < 0, "is negative")
-    combined = pred1 | pred2
 
-    assert combined(1) is True, "ORed predicate did not return the expected value."
-    assert combined(-1) is True, "ORed predicate did not return the expected value."
-    assert combined(0) is False, "ORed predicate did not return the expected value."
+    combined = pred1 | pred2
+    assert combined(1) is True
+    assert combined(-1) is True
+    assert combined(0) is False
     assert combined.render_msg() == "(is positive) or (is negative)"
 
 
 def test_invert() -> None:
     pred = Predicate(is_pos, "is positive")
-    inverted = ~pred
 
-    assert inverted(1) is False, "Inverted predicate did not return the expected value."
-    assert inverted(-1) is True, "Inverted predicate did not return the expected value."
+    inverted = ~pred
+    assert inverted(1) is False
+    assert inverted(-1) is True
     assert inverted.render_msg() == "not (is positive)"
+
+
+def test_xor() -> None:
+    pred1 = Predicate(is_pos, "is positive")
+    pred2 = Predicate[int](lambda x: x < 0, "is negative")
+
+    combined = pred1 ^ pred2
+    assert combined(1) is True
+    assert combined(-1) is True
+    assert combined(0) is False
+
+
+def test_implies() -> None:
+    pred1 = Predicate(is_pos, "is positive")
+    pred2 = Predicate[int](lambda x: x > 3, "greater than 3")
+
+    combined = pred1.implies(pred2)
+    assert combined(4) is True
+    assert combined(1) is False
+    assert combined(-1) is True
 
 
 if __name__ == "__main__":
